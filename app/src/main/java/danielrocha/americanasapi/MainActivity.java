95e47460cbe5bc6baca91fb80c7db62fcf8651ea
Daniel de Almeida Rocha <rocha.daniel52@gmail.com>
2016-11-28 08:36:19 -0200

    retirando butterknife do recyclerview e fazendo via databinding setando click dos itens do recyclerview

--- app/src/main/java/danielrocha/americanasapi/MainActivity.java
+++ app/src/main/java/danielrocha/americanasapi/MainActivity.java
@@ -1,5 +1,9 @@
 package danielrocha.americanasapi;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
@@ -14,6 +18,7 @@ import java.util.ArrayList;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import danielrocha.americanasapi.adapters.ProductsListAdapter;
+import danielrocha.americanasapi.databinding.MainBinding;
 import danielrocha.americanasapi.models.ProductModel;
 import danielrocha.americanasapi.services.ADSProductsAPI;
 import danielrocha.americanasapi.services.ServiceGenerator;
@@ -25,29 +30,16 @@ import retrofit2.Response;
 public class MainActivity extends AppCompatActivity {
 
     private final String TAGLOG = getClass().getName();
-    private LinearLayoutManager linearLayoutManager;
     private ProductsListAdapter productsListAdapter;
-    @Bind(R.id.recycler_view)
-    RecyclerView recyclerView;
+    private MainBinding mainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        ButterKnife.bind(this);
-
-        initRecyclerView();
+        mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
         getAmericanasADSProducts();
-    }
-
-    private void initRecyclerView() {
-        linearLayoutManager = new LinearLayoutManager(
-                MainActivity.this,
-                LinearLayoutManager.VERTICAL, false);
-        recyclerView.setLayoutManager(linearLayoutManager);
-        recyclerView.setItemAnimator(new DefaultItemAnimator());
     }
 
     private void getAmericanasADSProducts() {
@@ -68,11 +60,13 @@ public class MainActivity extends AppCompatActivity {
                     productsListAdapter = new ProductsListAdapter(response.body(), new OnItemClickListener() {
                         @Override
                         public void onItemClick(View view, ProductModel productModel) {
-                            Toast.makeText(MainActivity.this, "teste", Toast.LENGTH_SHORT).show();
+                            String url = getUrlFromModel(productModel);
+                            if (!url.startsWith("http://") && !url.startsWith("https://")) url = "http://" + url;
+                            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                            startActivity(browserIntent);
                         }
                     });
-                    recyclerView.setAdapter(productsListAdapter);
-                    //Log.i(TAGLOG, "encontrados: " + result.size());
+                    mainBinding.recyclerView.setAdapter(productsListAdapter);
                 }
             }
 
@@ -82,5 +76,16 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private String getUrlFromModel(ProductModel productModel) {
+        String result = null;
+
+        String[] vetor = productModel.getUrl().split("redirect_url=");
+        if(vetor.length == 2) {
+            result = vetor[1].replaceAll("%3A", ":").replaceAll("%2F", "/").replaceAll("%3F", "?");
+        }
+
+        return result;
     }
 }
---
+++ app/src/main/java/danielrocha/americanasapi/adapters/LayoutManagerBiding.java
@@ -0,0 +1,35 @@
+package danielrocha.americanasapi.adapters;
+
+import android.databinding.BindingAdapter;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by danielrocha on 28/11/16.
+ */
+
+public class LayoutManagerBiding {
+    @BindingAdapter({"bind:layout_manager"})
+    public static void setLayoutManager(
+            RecyclerView recyclerView, String layout){
+        setLayoutManager(recyclerView, layout, 1);
+    }
+
+    @BindingAdapter({"bind:layout_manager", "bind:columns"})
+    public static void setLayoutManager(
+            RecyclerView recyclerView,
+            String layout, int columns){
+
+        if ("linear".equalsIgnoreCase(layout)){
+            recyclerView.setLayoutManager(
+                    new LinearLayoutManager(
+                            recyclerView.getContext(),
+                            LinearLayoutManager.VERTICAL, false));
+        } else if ("grid".equalsIgnoreCase(layout)){
+            recyclerView.setLayoutManager(
+                    new GridLayoutManager(
+                            recyclerView.getContext(), columns));
+        }
+    }
+}
